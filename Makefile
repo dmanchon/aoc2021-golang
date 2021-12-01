b6244f5eccbb6d67020303fa234033173f84dee1
Daniel Manchon <dmanchon@gmail.com>
2021-12-01 12:03:07 +0100

    second part

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-.PHONY: day1-1
+.PHONY: day1-1 day1-2
 
 day1-1:
 	cat ./day1-1/input.txt | go run ./day1-1/main.go
+
+day1-2:
+	cat ./day1-2/input.txt | go run ./day1-2/main.go
---
+++ day1-2/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func window(lines []string) []int {
+	result := make([]int, 0)
+	var previous [3]*int
+
+loop:
+	for _, s := range lines {
+		x, err := strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
+
+		for i, prev := range previous {
+			if prev == nil {
+				previous[i] = &x
+				continue loop
+			}
+		}
+		sum := *previous[0] + *previous[1] + *previous[2]
+		result = append(result, sum)
+
+		previous[0] = previous[1]
+		previous[1] = previous[2]
+		previous[2] = &x
+	}
+
+	sum := *previous[0] + *previous[1] + *previous[2]
+	result = append(result, sum)
+	return result
+}
+
+func solve(lines []int) int {
+	increments := 0
+	var previous *int
+	for _, x := range lines {
+		x := x
+		if previous == nil {
+			fmt.Printf("%d (N/A - no previous measurement)\n", x)
+		} else if (x - *previous) > 0 {
+			fmt.Printf("%d (increased)\n", x)
+			increments += 1
+		} else {
+			fmt.Printf("%d (decreased)\n", x)
+		}
+		previous = &x
+	}
+	return increments
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	sums := window(lines)
+	solution := solve(sums)
+	fmt.Println(solution)
+}
