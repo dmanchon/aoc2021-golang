407fcc1fd0f9f5d0c2e6a8ef125d803804a161b2
Daniel Manchon <daniel@onna.com>
2021-12-08 17:48:04 +0100

    play with goroutinees

--- day8-2/main.go
+++ day8-2/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type RuneSet map[rune]struct{}
@@ -65,98 +66,116 @@ func Filter(list []RuneSet, size int) []RuneSet {
 }
 
 func Solve(lines []string) int {
-
 	sums := 0
+
+	results := make(chan int, len(lines))
+	var wg sync.WaitGroup
+
+	wg.Add(len(lines))
 	for _, line := range lines {
+		sums += SolveLine(line)
+		go func(line string) {
+			results <- SolveLine(line)
+			wg.Done()
+		}(line)
+	}
+	wg.Wait()
+	close(results)
 
-		split := strings.Split(line, "|")
-		digits := strings.Split(split[0], " ")
+	for r := range results {
+		sums += r
+	}
+	return sums
+}
 
-		var zero, one, two, three, four, five, six, seven, eight, nine RuneSet
-		list := make([]RuneSet, len(digits))
-		for _, digit := range digits {
-			s := BuildSet(digit)
-			list = append(list, s)
-		}
+func SolveLine(line string) int {
+	split := strings.Split(line, "|")
+	digits := strings.Split(split[0], " ")
 
-		one = Filter(list, 2)[0]
-		eight = Filter(list, 7)[0]
-		seven = Filter(list, 3)[0]
-		four = Filter(list, 4)[0]
-
-		for _, candidate := range Filter(list, 6) {
-			size := candidate.Difference(one).Len()
-			if size == 5 {
-				six = candidate
-			}
-		}
-
-		for _, candidate := range Filter(list, 5) {
-			size := candidate.Difference(one).Len()
-			if size == 3 {
-				three = candidate
-			}
-		}
-
-		for _, candidate := range Filter(list, 6) {
-			if candidate.Equal(six) {
-				continue
-			}
-			size := three.Difference(candidate).Len()
-			if size == 0 {
-				nine = candidate
-			} else {
-				zero = candidate
-			}
-		}
-
-		for _, candidate := range Filter(list, 5) {
-			if candidate.Equal(three) {
-				continue
-			}
-			size := six.Difference(candidate).Len()
-			if size == 2 {
-				two = candidate
-			} else if size == 1 {
-				five = candidate
-			}
-		}
-
-		number := make([]rune, 0)
-		for _, digit := range strings.Split(split[1], " ")[1:] {
-			s := BuildSet(digit)
-
-			if zero.Equal(s) {
-				number = append(number, '0')
-			} else if one.Equal(s) {
-				number = append(number, '1')
-			} else if two.Equal(s) {
-				number = append(number, '2')
-			} else if three.Equal(s) {
-				number = append(number, '3')
-			} else if four.Equal(s) {
-				number = append(number, '4')
-			} else if five.Equal(s) {
-				number = append(number, '5')
-			} else if six.Equal(s) {
-				number = append(number, '6')
-			} else if seven.Equal(s) {
-				number = append(number, '7')
-			} else if eight.Equal(s) {
-				number = append(number, '8')
-			} else if nine.Equal(s) {
-				number = append(number, '9')
-			} else {
-				number = append(number, '?')
-			}
-
-		}
-
-		n, _ := strconv.Atoi(string(number))
-		sums += n
+	var zero, one, two, three, four, five, six, seven, eight, nine RuneSet
+	list := make([]RuneSet, len(digits))
+	for _, digit := range digits {
+		s := BuildSet(digit)
+		list = append(list, s)
 	}
 
-	return sums
+	one = Filter(list, 2)[0]
+	eight = Filter(list, 7)[0]
+	seven = Filter(list, 3)[0]
+	four = Filter(list, 4)[0]
+
+	for _, candidate := range Filter(list, 6) {
+		size := candidate.Difference(one).Len()
+		if size == 5 {
+			six = candidate
+		}
+	}
+
+	for _, candidate := range Filter(list, 5) {
+		size := candidate.Difference(one).Len()
+		if size == 3 {
+			three = candidate
+			break
+		}
+	}
+
+	for _, candidate := range Filter(list, 6) {
+		if candidate.Equal(six) {
+			continue
+		}
+		size := three.Difference(candidate).Len()
+		if size == 0 {
+			nine = candidate
+		} else {
+			zero = candidate
+		}
+	}
+
+	for _, candidate := range Filter(list, 5) {
+		if candidate.Equal(three) {
+			continue
+		}
+		size := six.Difference(candidate).Len()
+		if size == 2 {
+			two = candidate
+		} else if size == 1 {
+			five = candidate
+		}
+	}
+
+	number := make([]rune, 0)
+	for _, digit := range strings.Split(split[1], " ")[1:] {
+		s := BuildSet(digit)
+
+		if zero.Equal(s) {
+			number = append(number, '0')
+		} else if one.Equal(s) {
+			number = append(number, '1')
+		} else if two.Equal(s) {
+			number = append(number, '2')
+		} else if three.Equal(s) {
+			number = append(number, '3')
+		} else if four.Equal(s) {
+			number = append(number, '4')
+		} else if five.Equal(s) {
+			number = append(number, '5')
+		} else if six.Equal(s) {
+			number = append(number, '6')
+		} else if seven.Equal(s) {
+			number = append(number, '7')
+		} else if eight.Equal(s) {
+			number = append(number, '8')
+		} else if nine.Equal(s) {
+			number = append(number, '9')
+		} else {
+			number = append(number, '?')
+		}
+
+	}
+
+	n, _ := strconv.Atoi(string(number))
+
+	return n
 }
 
 func ReadInput(file *os.File) []string {
